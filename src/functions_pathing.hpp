a20e923ea950b80d31ca94b2b7b40d4c4a1ea60b
NodmGatall <oliverheidmann@hotmail.de>
2015-11-07 00:24:12 +0100

    added spiral path

--- src/functions_pathing.hpp
+++ src/functions_pathing.hpp
@@ -17,11 +17,17 @@ glm::vec2 scythe_path(glm::vec2 direction, float speed, float current_lifetime)
     return direction;
 }
 
+glm::vec2 spiral_path(glm::vec2 direction, float speed, float current_lifetime)
+{
+    direction.x = speed * current_lifetime;
+    direction.y = 10 * sin(current_lifetime) / current_lifetime; 
+    return direction;
+}
+
 glm::vec2 sin_path(glm::vec2 direction, float speed, float current_lifetime)
 {
     return glm::vec2(0.f,0.f);
 }
-
 
 
 #endif
