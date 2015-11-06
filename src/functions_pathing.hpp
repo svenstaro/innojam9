89dc28946b9075300066e89cc081ef07f05441a7
NodmGatall <oliverheidmann@hotmail.de>
2015-11-06 23:20:04 +0100

    fixed bug in pathing functions

--- src/functions_pathing.hpp
+++ src/functions_pathing.hpp
@@ -19,7 +19,7 @@ glm::vec2 scythe_path(glm::vec2 direction, float speed, float current_lifetime)
 
 glm::vec2 sin_path(glm::vec2 direction, float speed, float current_lifetime)
 {
-
+    return glm::vec2(0.f,0.f);
 }
 
 
