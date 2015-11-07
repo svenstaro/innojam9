4ffd5ae9e96b1ac4925227ce15e09a91328f60ad
NodmGatall <oliverheidmann@hotmail.de>
2015-11-07 10:41:04 +0100

    added sin path

--- src/functions_pathing.hpp
+++ src/functions_pathing.hpp
@@ -3,6 +3,7 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtx/string_cast.hpp>
+#include "glm/gtx/optimum_pow.hpp"
 
 glm::vec2 linear_path(glm::vec2 direction, float speed , float current_lifetime)
 {
@@ -13,20 +14,22 @@ glm::vec2 linear_path(glm::vec2 direction, float speed , float current_lifetime)
 glm::vec2 scythe_path(glm::vec2 direction, float speed, float current_lifetime)
 {
     direction.x = speed * current_lifetime;
-    direction.y = sin(current_lifetime) + 45;
+    direction.y += sin(current_lifetime) + 45;
     return direction;
 }
 
 glm::vec2 spiral_path(glm::vec2 direction, float speed, float current_lifetime)
 {
     direction.x = speed * current_lifetime;
-    direction.y = 10 * sin(current_lifetime) / current_lifetime; 
+    direction.y += 10 * sin(current_lifetime) ; 
     return direction;
 }
 
 glm::vec2 sin_path(glm::vec2 direction, float speed, float current_lifetime)
 {
-    return glm::vec2(0.f,0.f);
+    direction.x = speed * current_lifetime;
+    direction.y += sin(current_lifetime * 4) / (current_lifetime * 4); 
+    return direction;
 }
 
 #endif
