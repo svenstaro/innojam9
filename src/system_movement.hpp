3a7f2873eb62fdf9a85ce13b0b3a1dc6e0785fb1
Sven-Hendrik Haase <sh@lutzhaase.com>
2015-11-07 16:36:00 +0100

    Rename direction to velocity in PlayerInstructionEvent

--- src/system_movement.hpp
+++ src/system_movement.hpp
@@ -15,7 +15,7 @@ class MovementSystem : public entityx::System<MovementSystem>,
                        public entityx::Receiver<MovementSystem> {
   public:
     MovementSystem(float min_dist = 100, float max_dist = 400)
-        : m_min_dist(min_dist), m_max_dist(max_dist){
+        : m_min_dist(min_dist), m_max_dist(max_dist) {
     }
 
     void configure(entityx::EventManager &event_manager) override {
@@ -29,15 +29,14 @@ class MovementSystem : public entityx::System<MovementSystem>,
     void receive(const PlayerInstructionEvent &player_instruction_event) {
         auto copy = player_instruction_event;
         auto position = copy.m_entity.component<Position>();
-        auto new_position = position->position() + player_instruction_event.m_direction;
+        auto new_position = position->position() + player_instruction_event.m_velocity;
 
-        if(new_position[0] < m_min_dist){
+        if (new_position[0] < m_min_dist) {
             new_position[0] = m_min_dist;
-        }
-        else if(new_position[0] > m_max_dist){
+        } else if (new_position[0] > m_max_dist) {
             new_position[0] = m_max_dist;
         }
-        new_position[1] = glm::mod(2*glm::pi<float>() + new_position[1], 2*glm::pi<float>());
+        new_position[1] = glm::mod(2 * glm::pi<float>() + new_position[1], 2 * glm::pi<float>());
         position->set_position(new_position);
     }
 
