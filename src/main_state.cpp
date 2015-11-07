85d9866565209ad36b2a74423a44065d2879ccec
Sven-Hendrik Haase <sh@lutzhaase.com>
2015-11-07 14:10:05 +0100

    Fix emscripten build

--- src/main_state.cpp
+++ src/main_state.cpp
@@ -20,7 +20,13 @@
 #include "entityx/entityx.h"
 
 #include <SDL.h>
+
+#ifdef __EMSCRIPTEN__
+#include <SDL/SDL_mixer.h>
+#else
 #include <SDL_mixer.h>
+#endif
+
 #include <glm/gtc/constants.hpp>
 
 MainState::MainState(Game *game) : m_game(game) {
