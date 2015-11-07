2d8d61ac8cdd0cba32d46e5abe54def48208bf21
Alexander Dmitriev <alexander.dmitriev2580@gmail.com>
2015-11-07 04:39:51 +0100

    fixed rotation of field, only minor fixes missing

--- src/system_draw.hpp
+++ src/system_draw.hpp
@@ -14,6 +14,8 @@
 #include "strapon/resource_manager/resource_manager.hpp"
 #include "strapon/sdl_helpers/sdl_helpers.hpp"
 
+#include<iostream>
+
 class DrawSystem : public entityx::System<DrawSystem> {
   public:
     DrawSystem(Game *game) : m_game(game) {
@@ -22,7 +24,11 @@ class DrawSystem : public entityx::System<DrawSystem> {
         m_camera = SDL_Rect{0, 0, w, h};
         m_drawtex =
             SDL_CreateTexture(game->renderer(), SDL_PIXELTYPE_UNKNOWN, SDL_TEXTUREACCESS_TARGET,
-                              game->world_size().w, game->world_size().h);
+                game->world_size().w, game->world_size().h);
+                              
+        m_maptex =
+            SDL_CreateTexture(game->renderer(), SDL_PIXELTYPE_UNKNOWN, SDL_TEXTUREACCESS_TARGET,
+                game->world_size().w, game->world_size().h);
     }
 
     ~DrawSystem() {
@@ -35,78 +41,120 @@ class DrawSystem : public entityx::System<DrawSystem> {
         cart.y = glm::sin(i[1]) * i[0];
         return cart;
     }
+    
+    inline double rad_to_deg(double f)
+    {
+      return f / (2.0*3.14159265)*360.0; //TODO 2 PI
+    }
+    
+    void render_entity(entityx::Entity& e, int woff, int hoff) {
+      auto drawable = e.component<Drawable>();
+      auto position = e.component<Position>(); //bad name -> change to.. polarpos?
+      
+      glm::vec2 coord_euclid = polar_to_euclid(position->position());
+
+      // Copy the coordinates to dest 
+      // and offset them by half the image size
+      SDL_Rect dest;
+      dest.x = coord_euclid.x - drawable->width()/2 + woff;
+      dest.y = coord_euclid.y - drawable->height()/2 + hoff;
+
+      dest.w = drawable->width();
+      dest.h = drawable->height();
+
+      SDL_Texture* tex = m_game->res_manager().texture(drawable->texture_key());
+      SDL_RenderCopyEx(m_game->renderer(), tex, nullptr, &dest, 
+          -rad_to_deg(position->position().y), nullptr, SDL_FLIP_NONE);
+    
+    }
 
     void update(entityx::EntityManager &es, entityx::EventManager &events,
                 entityx::TimeDelta dt) override {
-
-        // Change to render into rendertexture for now
-        SDL_SetRenderTarget(m_game->renderer(), m_drawtex);
-        SDL_SetRenderDrawColor(m_game->renderer(), 0, 100, 200, 255);
-        SDL_RenderClear(m_game->renderer());
-
+        // so that we have less unreadable text
+        SDL_Renderer* rendr = m_game->renderer();
+        
+        //FIRST render everything which is NOT THE PLAYER to maptex
+        SDL_SetRenderTarget(rendr, m_maptex);
+        SDL_SetRenderDrawColor(rendr, 0, 100, 200, 255);
+        SDL_RenderClear(rendr);
+        
+        // GET the size of the texture so that we can center all drawables
+        // btw the texture is 4 times as large as the viewport
+        int woff, hoff;
+        SDL_QueryTexture(m_maptex, nullptr, nullptr, &woff, &hoff);
+        woff /= 2;
+        hoff /= 2;
+        
+        //Therefore we need these handlers
         entityx::ComponentHandle<Drawable> drawable;
         entityx::ComponentHandle<Position> position;
-        entityx::ComponentHandle<Player> player;
 
-        glm::vec2 player_pos;
-        for (entityx::Entity entity : es.entities_with_components(player, position)) {
-            player_pos = polar_to_euclid(entity.component<Position>()->position());
-        }
-
+        //and we need to sort all the drawables by layers
         std::set<int> layers;
         for (entityx::Entity entity : es.entities_with_components(drawable)) {
             (void)entity;
             layers.insert(drawable->layer());
         }
-
+        
+        entityx::Entity player_entity;
         for (auto layer : layers) {
-            for (entityx::Entity entity : es.entities_with_components(drawable, position)) {
-                if (drawable->layer() == layer) {
-                    auto coord_polar = entity.component<Position>();
-                    SDL_Rect dest;
-
-                    // now follow the player
-                    glm::vec2 coord_euclid = polar_to_euclid(coord_polar->position());
-
-                    // Converted position
-                    dest.x = coord_euclid[0];
-                    dest.y = coord_euclid[1];
-
-                    // Center on entity
-                    dest.x -= drawable->width() / 2;
-                    dest.y -= drawable->height() / 2;
-
-                    // Translate onto player. In fact will do this later. Too confusing w/o other
-                    // entities
-                    dest.x += m_camera.w / 2;
-                    dest.y += m_camera.h / 2;
-
-                    dest.x -= player_pos.x;
-                    dest.y -= player_pos.y;
-
-                    dest.w = drawable->width();
-                    dest.h = drawable->height();
-
-                    SDL_RenderCopyEx(m_game->renderer(),
-                                     m_game->res_manager().texture(drawable->texture_key()), NULL,
-                                     &dest, 0, NULL, SDL_FLIP_NONE);
-                }
+          for (entityx::Entity entity : es.entities_with_components(drawable, position)) {
+            if (drawable->layer() == layer && !entity.component<Player>()) {
+              render_entity(entity, woff, hoff);
             }
+            else if(entity.component<Player>())
+              player_entity = entity;
+          }
         }
         
-        std::ostringstream os;
-        os << "Score: " << (int)player->score;
-        SDL_Color c = { 200, 200, 200, 0 };
-        draw_text(m_game->renderer(), m_game->res_manager(), os.str(), "font20", 0, 0,  c);
+        /*glm::vec2 player_pos;
+        for (entityx::Entity entity : es.entities_with_components(player, position)) {
+            player_pos = polar_to_euclid(entity.component<Position>()->position());
+        }*/
+        
+        // change the rendertarget to the pre-backbuffer
+        SDL_SetRenderTarget(rendr, m_drawtex);
+        SDL_SetRenderDrawColor(rendr, 0, 100, 200, 255);
+        
+        auto player_pos = player_entity.component<Position>();
+        drawable = player_entity.component<Drawable>();
+        
+        
+        // copy a rotated version of the maptexture.
+        SDL_RenderCopyEx(rendr, m_maptex, nullptr, nullptr, rad_to_deg(player_pos->position().y), nullptr, SDL_FLIP_NONE);
+        
+        // Render Player (pretend he is at alpha=-PI/2)
+        glm::vec2 coord_polar = player_pos->position();
+        coord_polar.y = 3.14159265 / 2.0;
+        glm::vec2 coord_euclid = polar_to_euclid(coord_polar);
 
+        // Copy the coordinates to dest 
+        // and offset them by half the image size
+        SDL_Rect dest;
+        dest.x = coord_euclid.x - drawable->width()/2 + woff;
+        dest.y = coord_euclid.y - drawable->height()/2 + hoff;
+
+        dest.w = drawable->width();
+        dest.h = drawable->height();
+
+        SDL_Texture* tex = m_game->res_manager().texture(drawable->texture_key());
+        SDL_RenderCopy(rendr, tex, nullptr, &dest);
+        
+        // Print the score of the player
+        std::ostringstream os;
+        os << "Score: " << (int) 4;//player->score;
+        SDL_Color c = { 200, 200, 200, 0 };
+        draw_text(rendr, m_game->res_manager(), os.str(), "font20", 0, 0,  c);
+        
         // Render to final window
-        SDL_SetRenderTarget(m_game->renderer(), nullptr);
-        SDL_RenderCopy(m_game->renderer(), m_drawtex, &m_camera, nullptr);
-        SDL_RenderPresent(m_game->renderer());
+        SDL_SetRenderTarget(rendr, nullptr);
+        SDL_RenderCopy(rendr, m_drawtex, &m_camera, nullptr);
+        SDL_RenderPresent(rendr);
     }
 
   private:
     Game *m_game;
     SDL_Rect m_camera;
     SDL_Texture *m_drawtex;
+    SDL_Texture *m_maptex;
 };
