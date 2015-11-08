3137cbeef5d2dd4175d6cd6f5a68944f6a8a7a40
Alexander Dmitriev <alexander.dmitriev2580@gmail.com>
2015-11-08 02:33:57 +0100

    voided some vars

--- src/system_orbspawn.hpp
+++ src/system_orbspawn.hpp
@@ -28,6 +28,8 @@ class OrbSpawnSystem : public entityx::System<OrbSpawnSystem>,
   public:
     OrbSpawnSystem(Game *game, entityx::EntityManager &entities, float min_dist, float max_dist)
         : m_min_dist(min_dist), m_max_dist(max_dist), m_game(game), m_entities(entities) {
+        (void)m_min_dist;
+        (void)m_max_dist;
     }
 
     ~OrbSpawnSystem() {
