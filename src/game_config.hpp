cbe226b8682f8bfc6393a7d36d3493531091722c
Frederick Gnodtke <frederick@gnodtke.net>
2015-11-08 14:23:14 +0100

    no debug

--- src/game_config.hpp
+++ src/game_config.hpp
@@ -24,7 +24,7 @@ static const float MAX_SPEED = 280.f;
 static const float ACCELERATION_STEP = 0.2f;
 static const float DECELERATION_STEP = 0.15f;
 
-static const bool DEBUG = true;
-static const int DEBUG_START_LEVEL = 6; // index of first level
+static const bool DEBUG = false;
+static const int DEBUG_START_LEVEL = 0; // index of first level
 
 #endif
