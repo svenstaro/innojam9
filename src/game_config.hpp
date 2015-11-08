de8ab6a50ccb8962c1fd41a80db68ac2e152be2d
Frederick Gnodtke <frederick@gnodtke.net>
2015-11-08 14:08:33 +0100

    merge

--- src/game_config.hpp
+++ src/game_config.hpp
@@ -26,6 +26,6 @@ static const float DECELERATION_STEP = 0.15f;
 
 static const bool DEBUG = true;
 
-static const int DEBUG_START_LEVEL = 6; // index of first level
+static const int DEBUG_START_LEVEL = 0; // index of first level
 
 #endif
